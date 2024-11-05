0d061043c9533c5a7fe76015abef05b70e95f560
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-05 13:56:51 -0600

    Refactor JobsList component by removing unused state and simplifying scroll handling

--- components/cards/resume/JobList.jsx
+++ components/cards/resume/JobList.jsx
@@ -1,34 +1,16 @@
 'use client'
 
-import { useRef, useState } from 'react'
 import { resume } from '@/data/resume'
 import { motion } from 'framer-motion'
 
 import { JobItem } from '@/components/cards/resume/JobItem'
 
 export default function JobsList() {
-  const [scrollPosition, setScrollPosition] = useState(0)
-  const handleScroll = (e) => {
-    setScrollPosition(e.target.scrollTop)
-  }
-  const heightItem = 64
-
-  const containerref = useRef(null)
   return (
     <motion.div className="relative w-full">
-      <section
-        className="relative overflow-auto h-[192px] snap-y snap-proximity"
-        onScroll={handleScroll}
-        ref={containerref}
-      >
+      <section className="relative overflow-auto h-[192px] snap-y snap-proximity">
         {resume.map((role, roleIndex) => (
-          <JobItem
-            key={roleIndex}
-            role={role}
-            containerref={containerref}
-            scrollPosition={scrollPosition}
-            heightItem={heightItem}
-          />
+          <JobItem key={roleIndex} role={role} />
         ))}
       </section>
     </motion.div>
