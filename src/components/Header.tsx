1d28a9f1066a16dd7d2cca8a2e744a12f259e429
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-14 21:04:45 -0600

    add Header component with social media links and integrate into Layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import LinkedIn from "../icons/Linkedin";
+import Github from "../icons/Github";
+import X from "../icons/X";
+
+export function Header() {
+  return (
+    <header className="flex justify-between mt-10">
+      <h1 className="font-bold">gerardocruz.dev</h1>
+      <div className="flex items-center gap-5">
+        <a
+          href="https://www.linkedin.com/in/gerardo-cruz-9903a3266/"
+          target="_blank"
+        >
+          <LinkedIn />
+        </a>
+        <a href="https://github.com/gerardocrr" target="_blank">
+          <Github />
+        </a>
+        <a href="https://x.com/gerardocrrr" target="_blank">
+          <X />
+        </a>
+      </div>
+    </header>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Header } from "./Header";
 
 interface Props {
   children: ReactNode;
@@ -7,7 +8,7 @@ interface Props {
 export function Layout({ children }: Props) {
   return (
     <div className="container max-w-4xl mx-auto flex flex-col min-h-screen">
-      <header className="bg-red-500">header</header>
+      <Header />
       <main className="flex-grow">{children}</main>
       <footer>footer</footer>
     </div>
