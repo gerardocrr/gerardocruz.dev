662591aa7c7c76e86be4ad24aa791aa1ad829733
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-19 23:45:42 -0600

    refactor CardProject component layout and styling

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,7 +1,16 @@
 export function CardProject() {
   return (
-    <div>
-      <h1>Card project</h1>
-    </div>
+    <article className="bg-slate-400 p-3 rounded-md">
+      <div className="flex items-center justify-between mb-5">
+        <div className="flex">
+          <img src="/logo.ico" alt="" className="h-6 w-6" />
+          <h1>hola</h1>
+        </div>
+        <div>
+          <img src="/logo.ico" alt="" className="h-6 w-6" />
+        </div>
+      </div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
   );
 }
