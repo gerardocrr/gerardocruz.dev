304a275737ed3d97bbbc3c01236ac1babe38205f
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-20 23:04:28 -0600

    update MainPage to render CardProject components dynamically from projects data

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,14 @@
 import { CardProject } from "../components/CardProject";
+import { projects } from "../../data/projects";
 
 export function MainPage() {
   return (
     <section>
       <h1>Projects</h1>
       <div className="grid grid-cols-2 gap-5">
-        <CardProject />
-        <CardProject />
-        <CardProject />
+        {projects.map((project) => (
+          <CardProject project={project} key={project.id} />
+        ))}
       </div>
     </section>
   );
