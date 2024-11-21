3dd3a7d869cadba02e8b4511c413d0dd375a7a5c
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-20 23:04:01 -0600

    refactor CardProject component to accept project data as props and display dynamic content

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,16 +1,32 @@
-export function CardProject() {
+interface Project {
+  img: string;
+  title: string;
+  link: string;
+  description: string;
+  tags: string[];
+}
+
+export function CardProject({ project }: { project: Project }) {
   return (
     <article className="bg-slate-400 p-3 rounded-md">
       <div className="flex items-center justify-between mb-5">
         <div className="flex">
-          <img src="/logo.ico" alt="" className="h-6 w-6" />
-          <h1>hola</h1>
+          <img src="/logo.ico" alt="" className="h-6 w-6 mr-2" />
+          <h1>{project.title}</h1>
         </div>
         <div>
           <img src="/logo.ico" alt="" className="h-6 w-6" />
         </div>
       </div>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>{project.description}</p>
+      <div className="flex items-center">
+        <img src="/logo.ico" alt="" className="h-6 w-6 mr-2" />
+        {project.tags.map((tag) => (
+          <span className="cursor-default rounded-md border border-neutral-300 px-2 py-1 bg-neutral-800/60 text-neutral-300">
+            {tag}
+          </span>
+        ))}
+      </div>
     </article>
   );
 }
