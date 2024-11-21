f50f77b4a9b838631bec2e49c2a3521e4ab89fac
gerardocrr <gerardo2551.cr@gmail.com>
2024-11-20 22:36:42 -0600

    add projects data file with sample project entries

---
+++ data/projects.ts
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    img: "/logo.ico",
+    title: "test 1",
+    link: "gerardocruz.dev",
+    description: "test description",
+    tags: ["HTML", "JavaScript", "React"],
+  },
+  {
+    img: "/logo.ico",
+    title: "test 2",
+    link: "gerardocruz.dev2",
+    description: "test description2",
+    tags: ["HTML", "JavaScript", "React"],
+  },
+  {
+    img: "/logo.ico",
+    title: "test 3",
+    link: "gerardocruz.dev3",
+    description: "test description3",
+    tags: ["HTML", "JavaScript", "React"],
+  },
+];
