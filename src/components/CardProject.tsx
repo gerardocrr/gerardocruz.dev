1c20193a1e4b5f69f626ce5c053b28792e5189aa
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-03 20:26:52 -0600

    refactor CardProject component to use separate 'web' and 'github' fields for links and update layout

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,7 +1,10 @@
+import Github from "@/icons/Github";
+
 interface Project {
   img: string;
   title: string;
-  link: string;
+  web: string;
+  github: string;
   description: string;
   tags: string[];
 }
@@ -14,17 +17,16 @@ export function CardProject({ project }: { project: Project }) {
           <img src="/logo.ico" alt="" className="h-6 w-6 mr-2" />
           <h1>{project.title}</h1>
         </div>
-        <div>
-          <img src="/logo.ico" alt="" className="h-6 w-6" />
-        </div>
+        <a href={project.github}>
+          <Github />
+        </a>
       </div>
       <p>{project.description}</p>
       <div className="flex items-center">
-        <img src="/logo.ico" alt="" className="h-6 w-6 mr-2" />
         {project.tags.map((tag) => (
           <span
             key={tag}
-            className="cursor-default rounded-md border border-neutral-300 px-2 py-1 bg-neutral-800/60 text-neutral-300"
+            className="cursor-default rounded-md border text-xs px-2 py-1 bg-neutral-800/60 text-neutral-300"
           >
             {tag}
           </span>
