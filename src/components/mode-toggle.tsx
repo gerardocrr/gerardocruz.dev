e32dc6ebffeb1763f23294f219b137d5d87736b6
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-03 19:31:50 -0600

    add ThemeProvider and ModeToggle components for theme management

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,30 @@
+import { Moon, Sun, MonitorCog } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/components/theme-provider";
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => {
+        setTheme(
+          theme === "light" ? "dark" : theme === "dark" ? "system" : "light"
+        );
+      }}
+    >
+      {theme == "light" && (
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      )}
+      {theme == "dark" && (
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+      {theme == "system" && (
+        <MonitorCog className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
