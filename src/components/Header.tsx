0006f46bba7420ae3026559bcb74c55f184a0034
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-03 19:54:28 -0600

    refactor Header component to apply buttonVariants for social media links and adjust spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,42 @@
 import LinkedIn from "../icons/Linkedin";
 import Github from "../icons/Github";
 import X from "../icons/X";
+import { buttonVariants } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
 
 export function Header() {
   return (
     <header className="flex justify-between mt-10">
       <h1 className="font-bold">gerardocruz.dev</h1>
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-2">
         <a
+          className={buttonVariants({
+            variant: "ghost",
+            size: "icon",
+          })}
           href="https://www.linkedin.com/in/gerardo-cruz-9903a3266/"
           target="_blank"
         >
           <LinkedIn />
         </a>
-        <a href="https://github.com/gerardocrr" target="_blank">
+        <a
+          className={buttonVariants({
+            variant: "ghost",
+            size: "icon",
+          })}
+          href="https://github.com/gerardocrr"
+          target="_blank"
+        >
           <Github />
         </a>
-        <a href="https://x.com/gerardocrrr" target="_blank">
+        <a
+          className={buttonVariants({
+            variant: "ghost",
+            size: "icon",
+          })}
+          href="https://x.com/gerardocrrr"
+          target="_blank"
+        >
           <X />
         </a>
         <ModeToggle />
