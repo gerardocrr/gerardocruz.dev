c5f7b3f28dc602b7fb1d6568d2698cd6a1a45f16
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-03 19:42:10 -0600

    refactor ModeToggle component to simplify theme switching logic and improve icon rendering

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun, MonitorCog } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/components/theme-provider";
 
@@ -10,19 +10,13 @@ export function ModeToggle() {
       variant="ghost"
       size="icon"
       onClick={() => {
-        setTheme(
-          theme === "light" ? "dark" : theme === "dark" ? "system" : "light"
-        );
+        setTheme(theme === "light" ? "dark" : "light");
       }}
     >
-      {theme == "light" && (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      )}
-      {theme == "dark" && (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      )}
-      {theme == "system" && (
-        <MonitorCog className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {theme == "light" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
