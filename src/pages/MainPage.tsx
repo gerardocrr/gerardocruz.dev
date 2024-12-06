5191b510d486e105ab920714dde6d83d205ce953
gerardocrr <gerardo2551.cr@gmail.com>
2024-12-05 20:27:20 -0600

    add margin to project title and enhance grid responsiveness on MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,8 +4,8 @@ import { projects } from "../../data/projects";
 export function MainPage() {
   return (
     <section>
-      <h1>Projects</h1>
-      <div className="grid grid-cols-2 gap-5">
+      <h1 className="mb-5">Projects</h1>
+      <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5">
         {projects.map((project) => (
           <CardProject project={project} key={project.id} />
         ))}
